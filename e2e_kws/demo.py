b1435cdfc30c40d9136d413bc467f9afec3d761a
Ali Belbaraka <ali.belbaraka@gmail.com>
2020-01-23 15:38:30 +0100

    updating local demo

--- e2e_kws/demo.py
+++ e2e_kws/demo.py
@@ -9,6 +9,7 @@ from demo import *
 from postprocessing import compute_mfcc_frames, probability_smoothing, reduce_false_alarms
 import keras
 import IPython
+from IPython.display import display, Markdown
 from models import *
 import io
 
@@ -16,10 +17,14 @@ import io
 kws_sets = [['people', 'because', 'think', 'world'], ['something', 'different', 'actually','important'], 
             ['another', 'percent', 'problem', 'technology'], ['information', 'experience', 'government', 'computer']]
 
-path2_models = {1: {'cnn_parada': 'set1_models/cnn_parada_set1_5_epochs_new_db.h5', 'dnn': 'set1_models/dnn_set1_5_epochs_new_db.h5'},
-                2: {'cnn_parada': 'set2_models/cnn_parada_set2_4_epochs_new_db.h5', 'dnn': 'set2_models/dnn_set2_4_epochs_new_db.h5'},
-                3: {'cnn_parada': 'set3_models/cnn_parada_set3_4_epochs_new_db.h5', 'dnn': 'set3_models/dnn_set3_4_epochs_new_db.h5'},
-                4: {'cnn_parada': 'set4_models/cnn_parada_set4_3_epochs_new_db.h5', 'dnn': 'set4_models/dnn_set4_3_epochs_new_db.h5'}}
+path2_models = {1: {'cnn_parada': 'set1_models/cnn_parada_set1_5_epochs.h5', 
+                    'dnn': 'set1_models/dnn_set1_5_epochs.h5'},
+                2: {'cnn_parada': 'set2_models/cnn_parada_set2_4_epochs.h5', 
+                    'dnn': 'set2_models/dnn_set2_4_epochs.h5'},
+                3: {'cnn_parada': 'set3_models/cnn_parada_set3_4_epochs.h5', 
+                    'dnn': 'set3_models/dnn_set3_4_epochs.h5'},
+                4: {'cnn_parada': 'set4_models/cnn_parada_set4_3_epochs.h5', 
+                    'dnn': 'set4_models/dnn_set4_4_epochs.h5'}}
 
 path2data = '/Users/Belbaraka/Desktop/Swisscom/Thesis/Datasets/demo'
 
@@ -77,11 +82,13 @@ def extract_keyword_occurences(seg_preds, kw_label, window_dur=1.0, shift=0.1, s
             kw_occ.append((start, end))
         else:
             if  len(kw_occ)>0:
-                kw_occurences.append(sample(kw_occ, k=1)[0])
+                #kw_occurences.append(sample(kw_occ, k=1)[0])
+                kw_occurences.append(kw_occ[0])
                 kw_occ = []                
     if  len(kw_occ)>0:
-        kw_occurences.append(sample(kw_occ, k=1)[0])   
-       
+        #kw_occurences.append(sample(kw_occ, k=1)[0])   
+         kw_occurences.append(kw_occ[0])
+            
     # Second round of pruning
     temp_indices = []
     for i,kw_occ in enumerate(kw_occurences[:-1]):
@@ -147,29 +154,33 @@ def spot_keywords(user_id, _set, recorder, model_type='dnn', shift=0.001, w_smoo
     frame_dur = 1.0
     non_keyword_label = len(keywords)
     
-    print("[1/4]: Loading test audio file and computing MFCC frames")
+    display(Markdown('**[1/4]**: Loading test audio file and computing MFCC frames'))
+    #print("[1/4]: Loading test audio file and computing MFCC frames")
     if path2wav is None:
         fs, sig = process_recording(user_id, recorder, _set)
     else:
         fs, sig = wav.read(path2wav)
     sig_frames = compute_mfcc_frames(sig, fs, xdim=xdim, shift=shift, num_features=num_features, verbose=1)
     
-    print("[2/4]: Frame-level predictions")
+    display(Markdown('**[2/4]**: Frame-level predictions'))
+    #print("[2/4]: Frame-level predictions")
     y_pred = model.predict(sig_frames, verbose=1)
     
-    print("[3/4]: Post-processing and segment-level integration")
+    display(Markdown('**[3/4]**: Post-processing and segment-level integration'))
+    #print("[3/4]: Post-processing and segment-level integration")
     y_pred_modified = reduce_false_alarms(y_pred)
     y_pred_smoothed_post = probability_smoothing(y_pred_modified, w_smooth=w_smooth)
     y_prediction_smoothed_post = np.argmax(y_pred_smoothed_post, axis=1) 
     predicted_segments = frames2segments(y_prediction_smoothed_post, non_keyword_label, segment_size)
     
-    print("[4/4]: Extracting keyword occurrences\n")
+    display(Markdown('**[4/4]**: Extracting keyword occurrences\n'))
+    #print("[4/4]: Extracting keyword occurrences\n")
     
     print("*************************************")
     
     all_kw_occurrences = keywords_found(keywords, predicted_segments, window_dur=frame_dur, 
                                         shift=shift, segment_size=segment_size)
-    if len(all_kw_occurrences) == 0:
+    if len(all_kw_occurrences) <= 1:
         print('No keywords found')
         
     print("*************************************")
@@ -177,9 +188,22 @@ def spot_keywords(user_id, _set, recorder, model_type='dnn', shift=0.001, w_smoo
     return all_kw_occurrences, sig, fs
     
     
-def listen2kw(kw, all_kw_occurrences, _set, fs=16000):
+def listen2_all_kw(all_kw_occurrences, _set, sig, fs=16000):
     keywords = kws_sets[_set - 1]
-    kw_occurrences = list(filter(lambda x: x[0] == keywords.index(kw), all_kw_occurrences))[0][1]
     
-    return iter(kw_occurrences)
-    
+    for kw in keywords:
+        display(Markdown('***' + kw.capitalize() + '***'))
+        #print(kw.upper() + '\n')
+        filtered_occurrences = list(filter(lambda x: x[0] == keywords.index(kw), all_kw_occurrences))
+        if len(filtered_occurrences) == 0:
+            print('no occurrences found\n')
+            print("*************************************")
+
+        else:
+            kw_occurrences = filtered_occurrences[0][1]
+            for i, (start, end) in enumerate(kw_occurrences):
+                #print('occurrence n°' + str(i + 1))
+                IPython.display.display(IPython.display.Audio(sig[int(start*fs): int(end*fs)], rate=fs))
+            print('*************************************')
+
+        
